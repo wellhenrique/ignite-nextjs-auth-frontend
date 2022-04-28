f4d8420c203ece4d303a3133a21a8e97a68f0452
wellhenrique <welissonh80@gmail.com>
2022-04-28 11:39:42 -0300

    feat: adding token to cookies

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -1,4 +1,5 @@
 import Router from "next/router";
+import { setCookie } from "nookies";
 import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
 
@@ -46,7 +47,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
         }
       );
 
-      const { permissions, roles } = response.data;
+      const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
       setUser({
         email,
         permissions,
