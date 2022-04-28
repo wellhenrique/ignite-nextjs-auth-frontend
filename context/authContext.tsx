515ac0f566038b0bc1feeff4d93e77e99ffa49f6
wellhenrique <welissonh80@gmail.com>
2022-04-28 14:07:59 -0300

    feat: retrieving authentication state

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -1,6 +1,7 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
-import { setCookie } from "nookies";
-import { createContext, ReactNode, useState } from "react";
+import { parseCookies, setCookie } from "nookies";
+
 import { api } from "../services/api";
 
 type User = {
@@ -34,18 +35,28 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
 
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api.get("me").then((response) => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email,
+          permissions,
+          roles,
+        });
+      });
+    }
+  }, []);
+
   async function signIn({ email, password }: SignInCredentials) {
     try {
-      const response = await api.post(
-        "sessions",
-        {
-          email,
-          password,
-        },
-        {
-          headers: { "Access-Control-Allow-Origin": "*" },
-        }
-      );
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
 
       const { token, refreshToken, permissions, roles } = response.data;
 
@@ -63,6 +74,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles,
       });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
 
       Router.push("/dashboard");
     } catch (err) {
